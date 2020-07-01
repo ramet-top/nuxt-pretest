c161e16670e31354bcc7476852bcd6820d1f7870
TopFusion-dev <ramet.top@gmail.com>
2020-07-01 17:04:02 +0700

    เรียนรู้การส่ง props กับ emit ค่า

---
+++ components/TestProps.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    data: {{ value }}
+    <v-btn @click="onClickBtn">ClickChild</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: 'OK'
+    }
+  },
+
+  watch: {
+    value(newVal, oldVal) {
+      console.log(newVal, oldVal)
+    }
+  },
+
+  methods: {
+    onClickBtn() {
+      this.$emit('input', 'child')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/test1.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Default</h1>
+    <TestProps @input="parentFc" :value="text" />
+
+    <h1>V-Model test</h1>
+    <TestProps v-model="text" />
+
+    <v-btn @click="onClickBtn">ClickParent</v-btn>
+  </div>
+</template>
+
+<script>
+import TestProps from '~/components/TestProps'
+export default {
+  components: {
+    TestProps
+  },
+
+  data() {
+    return {
+      text: 'UU'
+    }
+  },
+
+  methods: {
+    parentFc(value) {
+      console.log('parentFc', value)
+      this.text = value
+    },
+
+    onClickBtn() {
+      this.text = 'Parent'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
